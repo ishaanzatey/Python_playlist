9e91af00f24925e66482d6081049860a743149eb
Ishan Zatey <ishaan.zate@gmail.com>
2025-11-03 20:08:10 +0530

    adding from 97 to 98, 99 is empty

---
+++ 97_py_to_exe/python_to_exe.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from time import *
+
+
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_Label.config(text=time_string)
+
+    day_string = strftime("%A")
+    day_Label.config(text=day_string)
+
+    date_string = strftime("%B %d, %Y")
+    date_Label.config(text=date_string)
+
+    window.after(1000,update)       # 1000 is in miliseconds, and the update is the function which is being called
+        
+        
+window = Tk()
+
+time_Label = Label(window, font=("Ariel",50),fg="#00FF00",bg="black")
+time_Label.pack()
+
+day_Label = Label(window, font=("Ariel",50),fg="yellow")
+day_Label.pack()
+
+date_Label = Label(window, font=("Ariel",50),fg="yellow")
+date_Label.pack()
+
+
+update()
+
+window.mainloop()
---
+++ 99_text_editor/text_editor.py
@@ -0,0 +1,13 @@
+import os
+from tkinter import *
+from tkinter import filedialog, colorchooser, font
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+
+def change_color():
+    pass
+
+
+def change_font(*args):
+    pass
